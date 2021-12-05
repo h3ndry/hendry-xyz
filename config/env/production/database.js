9d0a0ea413c9604e54c8c186127b20b3306eb0d9
Hendry <h3ndry@users.noreply.github.com>
2021-12-05 11:16:47 +0000

    Update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,28 +1,19 @@
 const parse = require('pg-connection-string').parse;
-
-// const { host, port, database, user, password } = parse(process.env.DATABASE_URL);
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-      options: {
-        ssl: true,
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
     },
+    debug: false,
   },
 });
- 
